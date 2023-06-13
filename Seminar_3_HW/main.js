8019c69dff20659b837748658afc0c8ce047e046
«Alexa» <>
2023-06-14 00:47:35 +0300

    Seminar_3 HW added

---
+++ Seminar_3_HW/main.js
@@ -0,0 +1,5 @@
+        function showName() {
+            const name = prompt("Как вас зовут?", "Введите имя");
+            alert(`Привет, ${name}, пусть сегодня все будет супер!`);
+        }
+        showName()
